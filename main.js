f82836c83fded8ce00701a7b9b6bcaab47e6160e
Snehangi Behera <snehabehera200@gmail.com>
2025-03-19 13:45:05 +0530

    First Commit

---
+++ main.js
@@ -0,0 +1,54 @@
+
+const menuBtn = document.getElementById("menu_btn");
+const navLinks = document.getElementById("nav_links");
+const menuBtnIcon = menuBtn.querySelector("i");
+
+menuBtn.addEventListener("click", (e) => {
+    navLinks.classList.toggle("open");
+
+    const isOpen = navLinks.classList.contains("open");
+    menuBtnIcon.setAttribute("class", isOpen? "fa-solid fa-x" : "fa-solid fa-bars")
+})
+
+navLinks.addEventListener("click", (e) => {
+    navLinks.classList.remove("open");
+    menuBtnIcon.setAttribute("class", "fa-solid fa-bars");
+});
+
+const scrollRevealOption = {
+    distance : "50px",
+    origin: "bottom",
+    duration: 1000,
+};
+
+ScrollReveal().reveal(".container-left h1", {
+    ...scrollRevealOption,
+});
+ScrollReveal().reveal(".container-left .container-btn", {
+    ...scrollRevealOption,
+    delay:500,
+});
+ScrollReveal().reveal(".container-right h4", {
+    ...scrollRevealOption,
+    delay:2000,
+});
+ScrollReveal().reveal(".container-right h2", {
+    ...scrollRevealOption,
+    delay:2500,
+});
+ScrollReveal().reveal(".container-right h3", {
+    ...scrollRevealOption,
+    delay:3000,
+});
+ScrollReveal().reveal(".container-right p", {
+    ...scrollRevealOption,
+    delay:3500,
+});
+ScrollReveal().reveal(".container-right .nat-1", {
+    duration: 1000,
+    delay:4000,
+});
+ScrollReveal().reveal(".container-right .nat-2", {
+    duration: 1000,
+    delay:4500,
+});
